bfa1cb6a926cf77427e8eca48fd64ea2b1287895
Gerardo Cardenas <gerardo@gerardobsd.com>
2018-11-16 23:14:21 -0800

    Move user functions to user.py file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_restful import reqparse, abort, Api, Resource
 from db import *
+from user import *
 from peewee import *
 from flask_security import Security, PeeweeUserDatastore, UserMixin, RoleMixin, login_required, auth_token_required, current_user
 import os
@@ -15,34 +16,6 @@ user_datastore = PeeweeUserDatastore(db, Person, Role, UserRoles)
 security = Security(application, user_datastore)
 
 parser = reqparse.RequestParser(bundle_errors=True)
-
-class User(Resource):
-    @auth_token_required
-    def get(self):
-        return jsonify({"message": {"username": current_user.username}})
-    def post(self):
-        parser.add_argument('username', type=str, location='json', required=True)
-        parser.add_argument('password', type=str, location='json', required=True)
-        parser.add_argument('email', type=str, location='json', required=True)
-        args = parser.parse_args()
-        print args
-        username = args['username']
-        password = args['password']
-        email = args['email']
-
-        if password == '':
-            response = jsonify({"message": {"password":"Password can't be empty."}})
-            response.status_code = 400
-            return response
-
-        try:
-            if username and password and email:
-                user_datastore.create_user(email=email, password=password, username=username)
-                return {'created': username}
-        except IntegrityError:
-            response = jsonify({"message": {"error":"Username already exists."}})
-            response.status_code = 400
-            return response
 
 class Login(Resource):
     def post(self):
--- user.py
+++ user.py
@@ -1,9 +1,32 @@
-import json
-import falcon
+from flask import Flask, jsonify
+from flask_restful import reqparse, abort, Api, Resource
+from flask_security import Security, PeeweeUserDatastore, UserMixin, RoleMixin, login_required, auth_token_required, current_user
 
-class Resource(object):
-    def on_get(self, req, resp, name):
-        doc = {"user":"hi, {0}".format(name)}
 
-        resp.body = json.dumps(doc)
-        resp.status = falcon.HTTP_200
+class User(Resource):
+    @auth_token_required
+    def get(self):
+        return jsonify({"message": {"username": current_user.username}})
+    def post(self):
+        parser.add_argument('username', type=str, location='json', required=True)
+        parser.add_argument('password', type=str, location='json', required=True)
+        parser.add_argument('email', type=str, location='json', required=True)
+        args = parser.parse_args()
+        print args
+        username = args['username']
+        password = args['password']
+        email = args['email']
+
+        if password == '':
+            response = jsonify({"message": {"password":"Password can't be empty."}})
+            response.status_code = 400
+            return response
+
+        try:
+            if username and password and email:
+                user_datastore.create_user(email=email, password=password, username=username)
+                return {'created': username}
+        except IntegrityError:
+            response = jsonify({"message": {"error":"Username already exists."}})
+            response.status_code = 400
+            return response

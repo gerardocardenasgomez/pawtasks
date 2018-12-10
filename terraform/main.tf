2f618c28e5c41687d31ca177a70ce2fcd5cdc866
Gerardo Cardenas <gerardo@gerardobsd.com>
2018-12-09 17:55:55 -0800

    Add postgres terraform code

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+data "docker_registry_image" "postgres" {
+    name = "gerardocardenasgomez/pawtasks-db:1.0.0"
+}
+
+resource "docker_image" "postgres" {
+    name = "${data.docker_registry_image.postgres.name}"
+    pull_triggers = ["${data.docker_registry_image.postgres.sha256_digest}"]
+}
+
+resource "docker_network" "private_network" {
+    name = "pawtasksdev"
+}
+
+resource "docker_container" "postgres" {
+    name = "pawtasks_db"
+    image = "${docker_image.postgres.latest}"
+    networks_advanced {
+        name = "pawtasksdev"
+    }
+
+    ports {
+        internal = "5432"
+        ip = "10.12.0.50"
+        external = "5432"
+    }
+
+    env = [
+        "PAWTASKS_DB_ENV=${var.PAWTASKS_DB_ENV}",
+        "PAWTASKS_DB_USER=${var.PAWTASKS_DB_USER}",
+        "PAWTASKS_DB_PASSWORD=${var.PAWTASKS_DB_PASSWORD}",
+        "PAWTASKS_DB_HOSTNAME=${var.PAWTASKS_DB_HOSTNAME}",
+        "PAWTASKS_DB_PORT=${var.PAWTASKS_DB_PORT}",
+        "PAWTASKS_SECRET_KEY=${var.PAWTASKS_SECRET_KEY}"
+    ]
+
+}
+
+resource "null_resource" "postgres_db" {
+    depends_on = ["docker_container.postgres"]
+
+    provisioner "local-exec" {
+        command = "psql -U postgres -h ${docker_container.postgres.ports.0.ip} -c \"CREATE DATABASE ${var.PAWTASKS_DB_ENV}\""
+    }
+}
+
+resource "null_resource" "postgres_user" {
+    depends_on = ["docker_container.postgres"]
+
+    provisioner "local-exec" {
+        command = "psql -U postgres -h ${docker_container.postgres.ports.0.ip} -c \"ALTER USER ${var.PAWTASKS_DB_USER} WITH PASSWORD '${var.PAWTASKS_DB_PASSWORD}'\""
+    }
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "PAWTASKS_DB_ENV" {}
+variable "PAWTASKS_DB_USER" {}
+variable "PAWTASKS_DB_PASSWORD" {}
+variable "PAWTASKS_DB_HOSTNAME" {}
+variable "PAWTASKS_DB_PORT" {}
+variable "PAWTASKS_SECRET_KEY" {}
